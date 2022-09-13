d4da680a054b613bc35ae1f520c8c81658597250
Marko Andonov <marko.20112000@gmail.com>
2022-09-14 01:13:20 +0200

    task done

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios';
-import bootstrap from './assets/scss/app.scss';
+import './assets/scss/app.scss'
 import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false
